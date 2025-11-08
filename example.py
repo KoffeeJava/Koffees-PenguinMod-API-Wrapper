bf01c95e8bb30e629c470ab3c2f818b44a6169f8
KoffeeJava <85138593+KoffeeJava@users.noreply.github.com>
2025-11-08 11:26:43 -0700

    No token for you

---
+++ example.py
@@ -0,0 +1,8 @@
+# This is a example script!
+
+import kpaw
+
+KPAW.setUserAndToken("koffeejava", "")
+
+KPAW.getAmountMessage()
+print(f"Unread messages: {KPAW.Message_Amount}")
--- main.py
+++
@@ -1,6 +0,0 @@
-import KPAW
-
-KPAW.setUserAndToken("koffeejava", "16e6c1430dcd17089b5328a5afcce010164fd978651371aa8f791466bf94e528")
-
-KPAW.getAmountMessage()
-print(f"Unread messages: {KPAW.Message_Amount}")
